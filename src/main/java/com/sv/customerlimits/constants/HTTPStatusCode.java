6240f8e336ff3a9c6ac9c0487bfa25382a28b93e
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-07-07 16:35:33 -0400

    HTTP Status Constants

--- src/main/java/com/sv/customerlimits/constants/HTTPStatusCode.java
+++ src/main/java/com/sv/customerlimits/constants/HTTPStatusCode.java
@@ -1,2 +1,16 @@
-package com.sv.customerlimits.constants;public enum HTTPStatusCode {
+package com.sv.customerlimits.constants;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public enum HTTPStatusCode {
+    BAD_REQUEST;
+
+//    public ResponseEntity getResponse(String message) {
+//        switch (this) {
+//            case BAD_REQUEST:
+//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+//        }
+//        return null;
+//    }
 }
