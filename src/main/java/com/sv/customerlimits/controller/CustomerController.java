a6c652a45f1a6939aa4c6b5a1d2e33bfdf096724
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-07-09 14:14:38 -0400

    CRUD functionality for customer

--- src/main/java/com/sv/customerlimits/controller/CustomerController.java
+++ src/main/java/com/sv/customerlimits/controller/CustomerController.java
@@ -5,14 +5,12 @@ import com.sv.customerlimits.response.CustomerResponse;
 import com.sv.customerlimits.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/customers")
 public class CustomerController {
     private final CustomerService customerService;
 
@@ -21,14 +19,24 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @GetMapping("/api/customers")
+    @GetMapping
     public List<Customer> fetchAllCustomers() {
         return customerService.fetchAllCustomers();
     }
 
-    @PostMapping("/api/customers")
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> fetchCustomerById(@PathVariable(value = "id") Long customerId) {
+        return customerService.fetchCustomerById(customerId);
+    }
+
+    @PostMapping
     public ResponseEntity<Object> addNewCustomer(@RequestBody Customer customer) {
-        return customerService.addNewCustomer(customer);
+        return customerService.addNewCustomer(customer, customer.getLimitId());
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable(value = "id") Long customerId) {
+        customerService.removeCustomer(customerId);
     }
 
 }
--- src/main/java/com/sv/customerlimits/controller/TransactionLimitController.java
+++ src/main/java/com/sv/customerlimits/controller/TransactionLimitController.java
@@ -1,22 +1,14 @@
 package com.sv.customerlimits.controller;
 
-import com.sv.customerlimits.entity.Customer;
-import com.sv.customerlimits.entity.TransactionLimit;
 import com.sv.customerlimits.service.TransactionLimitService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TransactionLimitController {
 
-    private TransactionLimitService transactionLimitService;
-
     @Autowired
     public TransactionLimitController(TransactionLimitService transactionLimitService) {
-        this.transactionLimitService = transactionLimitService;
     }
 
 //    @PostMapping("/api/limits")
