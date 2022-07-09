db0ebc8f9634bfe7e328e27b057d2a06ca9b1826
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-07-09 14:17:20 -0400

    Save new customer - test

--- src/test/java/com/sv/customerlimits/dao/CustomerRepositoryTest.java
+++ src/test/java/com/sv/customerlimits/dao/CustomerRepositoryTest.java
@@ -14,11 +14,11 @@ class CustomerRepositoryTest {
 
     @Test
     public void saveNewCustomer() {
-        TransactionLimit limit = TransactionLimit.builder().dailyLimit(500).weeklyLimit(1500).monthlyLimit(5000).build();
-
-        Customer customer = Customer.builder().firstName("Joe").lastName("Sugg").email("joe@gmail.com").mobile("9231139933").transactionLimit(limit).build();
-
-        customerRepository.save(customer);
+//        TransactionLimit limit = TransactionLimit.builder().dailyLimit(500).weeklyLimit(1500).monthlyLimit(5000).build();
+//
+//        Customer customer = Customer.builder().firstName("Joe").lastName("Sugg").email("joe@gmail.com").mobile("9231139933").transactionLimit(limit).build();
+//
+//        customerRepository.save(customer);
     }
 
 }
